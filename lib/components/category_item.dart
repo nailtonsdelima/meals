c63dbca7f6dbc66fa66f0b554aa76a7bdb433a92
Nailton Santos <nailtonsdelima@gmail.com>
2025-04-19 11:15:48 -0300

    Iniciando navegação em novas telas e criando componentes para tela de receitas

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,30 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart';
 
+import 'package:meals/screens/categories_meals_screen.dart';
+
 class CategoryItem extends StatelessWidget {
   final Category category;
+
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return const CategoriesMealsScreen();
+        },
+      ),
+    );
+  }
 
   // ignore: use_key_in_widget_constructors
   const CategoryItem(this.category);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      // ignore: sort_child_properties_last
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.titleMedium,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.5),
-            category.color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        // ignore: sort_child_properties_last
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.5),
+              category.color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
       ),
     );
