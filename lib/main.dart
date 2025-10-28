a8f6d4d581fd96956f89cac5f83d525fd28b1a80
unknown <nailtonsdelima@gmail.com>
2025-10-28 16:29:42 -0300

    Adicionando tabs de navegação entre categorias e favoritos

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'screens/categories_screen.dart';
+import 'package:meals/screens/tabs_screen.dart';
+// import 'screens/categories_screen.dart';
 import 'screens/categories_meals_screen.dart';
 import 'screens/meal_detail_screen.dart';
 import 'Utils/app_routes.dart';
@@ -40,7 +41,7 @@ class MyApp extends StatelessWidget {
       ),
       // home: CategoriesScreen(),
       routes: {
-        AppRoutes.HOME: (ctx) => CategoriesScreen(),
+        AppRoutes.HOME: (ctx) => TabsScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
         AppRoutes.MEAL_DETAIL: (ctx) => const MealDetailScreen(),
       },
